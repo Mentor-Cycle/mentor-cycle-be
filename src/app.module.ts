6c5cd8d7dc2c0a3ef7c6f8596105099abe2615fc
Leonardo Balsalobre <oliveirabalsa2@gmail.com>
2023-02-01 18:08:35 -0300

    feat: add modules alias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
-//import { GraphQLModule } from '@modules/graphql'; //fix path
-import { GraphQLModule } from './modules/graphql';
-//import { PrismaModule } from '@modules/prisma'; //fix path
-import { PrismaModule } from './modules/prisma';
+import { GraphQLModule } from '@modules/graphql'; //fix path
+import { PrismaModule } from '@modules/prisma'; //fix path
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    GraphQLModule,
-    PrismaModule
-  ],
+  imports: [GraphQLModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/graphql/resolvers/any.resolver.ts
+++ src/modules/graphql/resolvers/any.resolver.ts
@@ -1,25 +1,22 @@
-import { Inject } from "@nestjs/common";
-import { Args, Resolver, Query, Mutation } from "@nestjs/graphql";
-//import { AnyService } from "@modules/services"; //fix path
-import { AnyService } from "../../../modules/services";
-import { TestModel } from "../models/test.model";
+import { Inject } from '@nestjs/common';
+import { Args, Resolver, Query, Mutation } from '@nestjs/graphql';
+import { AnyService } from '@modules/services';
+import { TestModel } from '../models/test.model';
 
 @Resolver(() => TestModel)
 export class AnyResolver {
-    constructor(
-        @Inject(AnyService) private anyService: AnyService
-    ) {}
+  constructor(@Inject(AnyService) private anyService: AnyService) {}
 
-    @Query(returns => TestModel)
-    async any(@Args('id') id: number): Promise<TestModel> {
-        return await this.anyService.findOne(id)
-    }
+  @Query((returns) => TestModel)
+  async any(@Args('id') id: number): Promise<TestModel> {
+    return await this.anyService.findOne(id);
+  }
 
-    @Mutation(returns => TestModel)
-    async createAny(
-        @Args('anyUniqueText') anyUniqueText: string,
-        @Args('anyNonUniqueText') anyNonUniqueText: string
-    ): Promise<TestModel> {
-        return await this.anyService.create({ anyUniqueText, anyNonUniqueText})
-    }
+  @Mutation((returns) => TestModel)
+  async createAny(
+    @Args('anyUniqueText') anyUniqueText: string,
+    @Args('anyNonUniqueText') anyNonUniqueText: string,
+  ): Promise<TestModel> {
+    return await this.anyService.create({ anyUniqueText, anyNonUniqueText });
+  }
 }
