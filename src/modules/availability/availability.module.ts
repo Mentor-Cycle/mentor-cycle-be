5c29733c139f8e1bb74e18d87ea46fd7da795e4e
Leonardo <oliveirabalsa2@gmail.com>
2023-03-17 18:06:02 -0300

    feat(authentication): add guardian and cookies

--- src/modules/availability/availability.module.ts
+++ src/modules/availability/availability.module.ts
@@ -1,9 +1,15 @@
+import { JwtService } from '@nestjs/jwt';
 import { UserRepository } from './../user/user.repository';
 import { Module } from '@nestjs/common';
 import { AvailabilityService } from './availability.service';
 import { AvailabilityResolver } from './availability.resolver';
 
 @Module({
-  providers: [AvailabilityResolver, AvailabilityService, UserRepository],
+  providers: [
+    AvailabilityResolver,
+    AvailabilityService,
+    UserRepository,
+    JwtService,
+  ],
 })
 export class AvailabilityModule {}
