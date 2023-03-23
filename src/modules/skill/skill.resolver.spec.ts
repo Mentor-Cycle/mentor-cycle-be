15659b57aa8e2b0edd8b79c751329e96d67764a7
Leonardo <oliveirabalsa2@gmail.com>
2023-03-23 17:10:27 -0300

    test(skills): add resolver

---
+++ src/modules/skill/skill.resolver.spec.ts
@@ -0,0 +1,38 @@
+import { HttpException } from '@nestjs/common';
+import { PrismaService } from '@modules/prisma';
+import { Test, TestingModule } from '@nestjs/testing';
+import { SkillRepository } from './skill.repository';
+import { SkillResolver } from './skill.resolver';
+import { SkillService } from './skill.service';
+
+describe('SkillResolver', () => {
+  let resolver: SkillResolver;
+  let service: SkillService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SkillResolver, SkillService, SkillRepository, PrismaService],
+    }).compile();
+
+    resolver = module.get<SkillResolver>(SkillResolver);
+    service = module.get<SkillService>(SkillService);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  describe('findAllSkills', () => {
+    it('should return an array of skills', async () => {
+      const skills = [{ name: 'Front-End', id: '1' }];
+      jest.spyOn(service, 'findAll').mockImplementation(async () => skills);
+
+      expect(await resolver.findAllSkills()).toBe(skills);
+    });
+    it('should throw an HttpException if no skills are found', async () => {
+      jest.spyOn(service, 'findAll').mockImplementation(async () => []);
+      await expect(resolver.findAllSkills()).rejects.toThrow(HttpException);
+    });
+  });
+});
