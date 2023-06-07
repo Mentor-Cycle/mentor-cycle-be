22643c7354493bb3047451a27a10b63761c0c6a7
Leonardo <oliveirabalsa2@gmail.com>
2023-06-06 23:55:51 -0300

    feat: implement send email on new event

--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { EventService } from './event.service';
 import { EventResolver } from './event.resolver';
 import { JwtService } from '@nestjs/jwt';
+import { MailService } from '@common/services/mail/mail.service';
 
 @Module({
-  providers: [EventResolver, EventService, JwtService],
+  providers: [EventResolver, EventService, JwtService, MailService],
 })
 export class EventModule {}
