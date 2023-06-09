490206ffbbd01b0f09b5ec7da0c65301ab2bf0d2
Diego Sano <diegocsano@outlook.com>
2023-06-09 14:00:28 -0300

    feat: create notification entity

---
+++ src/modules/notifications/entities/notification.entity.ts
@@ -0,0 +1,24 @@
+import { FieldId } from '@common/decorators';
+import { User } from '@modules/user/entities/user.entity';
+import { ObjectType, Field } from '@nestjs/graphql';
+
+@ObjectType()
+export class Notification {
+  @FieldId()
+  id: string;
+
+  @Field(() => String)
+  title: string;
+
+  @Field(() => String)
+  description: string;
+
+  @Field(() => String)
+  imageUrl: string;
+
+  @Field(() => Boolean)
+  read: boolean;
+
+  @Field(() => [User])
+  users: User[];
+}
