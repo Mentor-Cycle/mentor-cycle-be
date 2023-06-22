f8c4d6b5900013d873364fa73bac798bd6f4f3cc
Eric Oliveira <oiericoi@hotmail.co>
2023-06-22 17:13:18 -0300

    feat: add roles check in authguard

--- src/common/auth/auth.guard.ts
+++ src/common/auth/auth.guard.ts
@@ -1,15 +1,20 @@
 import { JwtService } from '@nestjs/jwt';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
   canActivate(context: ExecutionContext): boolean {
     const ctx = GqlExecutionContext.create(context);
     const { req, res } = ctx.getContext();
     if (!req.cookies.token) {
       return false;
+    }
+    const roles = this.reflector.get<string[]>('role', context.getHandler());
+    if (!roles) {
+      return true;
     }
     const { token } = req.cookies;
     const isVerified = this.jwtService.verify(token, {
@@ -22,7 +27,10 @@ export class AuthGuard implements CanActivate {
         secure: true,
         path: '/',
       });
+      return false;
     }
-    return !!isVerified;
+    const hasHole = roles.some(() => roles.includes(isVerified.role));
+
+    return !!isVerified && hasHole;
   }
 }
