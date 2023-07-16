b4ae96371ac99d95d1abb53de25a94a22078972f
GustavoCesarSantos <gustavocs789@gmail.com>
2023-07-16 20:06:21 -0300

    refactor(recommendation): add providers in module
    Signed-off-by: GustavoCesarSantos <gustavocs789@gmail.com>

--- src/modules/recommendation/recommendation.module.ts
+++ src/modules/recommendation/recommendation.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RecommendationResolver } from './recommendation.resolver';
+import { JwtService } from '@nestjs/jwt';
+import { RecommendationService } from './recommendation.service';
 
-@Module({})
+@Module({
+  providers: [RecommendationResolver, RecommendationService, JwtService],
+})
 export class RecommendationModule {}
