8e3101e76bfa4c92af808cf0e37246c7275a878d
GustavoCesarSantos <gustavocs789@gmail.com>
2023-07-16 20:07:37 -0300

    style(testimony): remove comment code
    Signed-off-by: GustavoCesarSantos <gustavocs789@gmail.com>

--- src/modules/testimony/entities/testimony.entity.ts
+++ src/modules/testimony/entities/testimony.entity.ts
@@ -1,5 +1,4 @@
 import { FieldId } from '@common/decorators';
-//import { User } from '@modules/user/entities/user.entity';
 import { ObjectType, Field, Directive } from '@nestjs/graphql';
 
 @ObjectType()
