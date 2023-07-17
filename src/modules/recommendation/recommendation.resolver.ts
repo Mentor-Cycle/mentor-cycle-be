bfb9d8d668a8c1b1a9436e5c7f12cfa30ecdfacd
GustavoCesarSantos <gustavocs789@gmail.com>
2023-07-16 22:12:18 -0300

    feat(recommendation): add update approved method
    Signed-off-by: GustavoCesarSantos <gustavocs789@gmail.com>

--- src/modules/recommendation/recommendation.resolver.ts
+++ src/modules/recommendation/recommendation.resolver.ts
@@ -4,6 +4,7 @@ import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@common/auth/auth.guard';
 import { CreateRecommendationInput } from './dto/create-recommendation.input';
 import { RecommendationService } from './recommendation.service';
+import { UpdateApprovedInput } from './dto/update-approved.input';
 
 @Resolver(() => Recommendation)
 export class RecommendationResolver {
@@ -16,5 +17,16 @@ export class RecommendationResolver {
     createRecommendationInput: CreateRecommendationInput,
   ) {
     return this.recommendationService.create(createRecommendationInput);
+  }
+
+  @UseGuards(AuthGuard)
+  @Mutation(() => Recommendation)
+  updateApproved(
+    @Args('updateApprovedInput') updateApprovedInput: UpdateApprovedInput,
+  ) {
+    return this.recommendationService.updateApproved(
+      updateApprovedInput.id,
+      updateApprovedInput.approved,
+    );
   }
 }
